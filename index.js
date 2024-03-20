53893533278373d41cdd7a20bc1bffcdeb751071
pyroshary <sheharyarghulamrasool@gmail.com>
2024-03-21 02:55:46 +0500

    commit

---
+++ index.js
@@ -0,0 +1,3 @@
+var number = [3, 3, 6, 2, 8, 10];
+var doublenumber = number.map(function (number) { return number * 5; });
+console.log(doublenumber);
---
+++ index.ts
@@ -0,0 +1,4 @@
+let number = [3,3,6,2,8,10];
+let doublenumber = number.map(number=> number*5);
+
+console.log(doublenumber);
